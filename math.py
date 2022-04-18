3c69e993c3d07c4b8f719a696efb32e11fd4b7a7
abbaxeahm <69853455+abbaxeahm@users.noreply.github.com>
2022-04-18 19:01:01 +0200

    Update math.py

--- math.py
+++ math.py
@@ -1,6 +1,41 @@
 from math import ceil
-total = [0.0, 0]
-addPerSec = [0.0, 0]
+
+
+class largeNumber:
+    def __init__(self, significand: float, exp: int):
+        self.significand = significand
+        self.exp = exp
+
+    def updateNum(self):
+        if self.significand == 0.0:
+            self.exp = 0
+        elif self.significand >= 1000:
+            self.significand /= 1000
+            self.exp += 1
+        elif self.significand < 1:
+            self.significand *= 1000
+            self.exp -= 1
+
+    def add(self, term):
+        self.significand += term.significand * pow(1000, term.exp - self.exp)
+
+    def sub(self, term):
+        print(self.significand)
+        print(term.significand)
+        if self.exp > term.exp or (self.significand >= term.significand and self.exp == term.exp):
+            self.significand -= (term.significand *
+                                 pow(1000, term.exp - self.exp))
+            return True
+        else:
+            return False
+
+    def overWrite(self, significand: float, exp: int):
+        self.significand = significand
+        self.exp = exp
+
+
+total = largeNumber(0.0, 0)
+addPerSec = largeNumber(0.0, 0)
 nameOfnumber10 = ["M", "B", "Tr", "Quadr",
                   "Quint", "Sext", "Sept", "Oct", "Non", "Dec"]
 
@@ -21,72 +56,53 @@ nameOfnumber = [["Un", "Duo", "Tre", "Quattuor", "Quinqua", "Se", "Septe", "Octo
 # 9	Nove(*)	Nonaginta	Nongenti
 
 
-def buy(price):
-    global total
-    if affordeble(price):
-        total[0] -= getSignificandOfTerm1Exp(total[1], price)
-
-
-def affordeble(price):
-    return total[1] > price[1] or total[0] >= price[0]
-
-
-def getSignificandOfTerm1Exp(term1Exp, term2):
-    return term2[0] * pow(1000, term2[1] - term1Exp)
-
-
-def updateNum(num):
-    if num[0] == 0.0:
-        return [0.0, 0]
-    elif num[0] >= 1000:
-        return [num[0] / 1000, num[1] + 1]
-    elif num[0] < 1:
-        return [num[0] * 1000, num[1] - 1]
-    else:
-        return num
+def buy(price: largeNumber):
+    if total.sub(price):
+        print("ny monster")
 
 
 def perSecUpdate():
     global total
     global addPerSec
-    total[0] += getSignificandOfTerm1Exp(total[1], addPerSec)  # * deltaTime(s)
-    total = updateNum(total)
-    addPerSec = updateNum(addPerSec)
+    total.add(addPerSec)  # * deltaTime(s)
+    total.updateNum()
+    addPerSec.updateNum()
     printNum(total)
     printNum(addPerSec)
 
 
-def printNum(num):
+def printNum(num: largeNumber):
     print(end="1000exp: ")
-    print(num[1])
+    print(num.exp)
     print(end="10exp: ")
-    print(num[1]*3)
-    if num[1] == 1:
-        print(int(round(1000 * num[0])))
-    elif num[1] == 0:
-        print("%.3f" % round(num[0], 3))
-    elif num[1] < 22:
-        print("%.3f" % round(num[0], 3), end=" ")
+    print(num.exp*3)
+    if num.exp == 1:
+        print(int(round(1000 * num.significand)))
+    elif num.exp == 0:
+        print("%.3f" % round(num.significand, 3))
+    elif num.exp < 22:
+        print("%.3f" % round(num.significand, 3), end=" ")
         # print(nameOftotalExp[num[1]])
-        print(nameOfnumber10[ceil((num[1]-1)/2)-1] +
-              ("illion" if num[1] % 2 == 0 else "illiard"))
-    elif num[1] < 2000:
-        print(ceil((num[1]+1)/2-1))
-        s = str(ceil((num[1]+1)/2-1))[::-1]
+        print(nameOfnumber10[ceil((num.exp-1)/2)-1] +
+              ("illion" if num.exp % 2 == 0 else "illiard"))
+    elif num.exp < 2000:
+        print(ceil((num.exp+1)/2-1))
+        s = str(ceil((num.exp+1)/2-1))[::-1]
         print(s)
+        print("%.3f" % round(num.significand, 3), end=" ")
         count = 0
         for c in s:
-            if int(d) > 0:
+            if int(c) > 0:
                 print(nameOfnumber[count][int(c)-1], end="")
             count += 1
-        print(("illion" if num[1] % 2 == 0 else "illiard"))
+        print(("illion" if num.exp % 2 == 0 else "illiard"))
     else:
         print("to large")
 
 
 while True:
-    addPerSec[0] = float(input("add per sec"))
-    addPerSec[1] = int(input("add per sec exp"))
+    addPerSec.overWrite(float(input("add per sec")),
+                        int(input("add per sec exp")))
     if input() != "":
-        buy([1, int(input("buy: "))])
+        buy(largeNumber(1, int(input("buy: "))))
     perSecUpdate()
